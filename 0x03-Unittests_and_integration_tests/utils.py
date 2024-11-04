e717c3086045ae0300dc5a0dafa908f3eca94353
Eugene Agesa <eugenethebandit@gmail.com>
2024-11-04 19:22:42 +0000

    unittests code

---
+++ 0x03-Unittests_and_integration_tests/utils.py
@@ -0,0 +1,74 @@
+
+#!/usr/bin/env python3
+"""
+Module for testing utils script.
+"""
+import requests
+from functools import wraps
+from typing import (
+    Mapping,
+    Sequence,
+    Any,
+    Dict,
+    Callable,
+)
+
+__all__ = [
+    "access_nested_map",
+    "get_json",
+    "memoize",
+]
+
+
+def access_nested_map(nested_map: Mapping, path: Sequence) -> Any:
+    """Accessing nested map with key path to
+    Parameters
+    nested_map: Mapping
+        Nested maps
+    path: Sequence
+        Sequence of keys representing path to value
+    
+    """
+    for key in path:
+        if not isinstance(nested_map, Mapping):
+            raise KeyError(key)
+        nested_map = nested_map[key]
+
+    return nested_map
+
+
+def get_json(url: str) -> Dict:
+    """Get JSON from remote URL.
+    """
+    response = requests.get(url)
+    return response.json()
+
+
+def memoize(fn: Callable) -> Callable:
+    """Decorator to memoize a method.
+    Example
+    -------
+    class MyClass:
+        @memoize
+        def a_method(self):
+            print("a_method called")
+            return 42
+    >>> my_object = MyClass()
+    >>> my_object.a_method
+    a_method called
+    42
+    >>> my_object.a_method
+    42
+    """
+    attr_name = "_{}".format(fn.__name__)
+
+    @wraps(fn)
+    def memoized(self):
+        """"memoized wraps"""
+        if not hasattr(self, attr_name):
+            setattr(self, attr_name, fn(self))
+        return getattr(self, attr_name)
+
+    return property(memoized)
+
+
